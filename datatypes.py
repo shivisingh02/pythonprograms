d32cc87e069d0c514796bb01a18c3a9373a143fa
shivisingh02 <111993202+shivisingh02@users.noreply.github.com>
2022-08-30 20:00:57 +0530

    Create datatypes.py

---
+++ datatypes.py
@@ -0,0 +1,38 @@
+x = 1233
+y = 34
+z = -345654
+print(x , type(x))
+print(y , type(y))
+print(z , type(z))
+x = 1.10
+y = 2.20
+z = 12.433
+print(x , type(x))
+print(y , type(y))
+print(z , type(z))
+name = "shivi"
+location = 'lucknow'
+colour = "red"
+print(name , type(name))
+print(location, type(location))
+print(colour , type(colour))
+want_togo = True
+eat = True
+want_tostudy = False
+print(want_togo , type(want_togo))
+print(eat , type(eat))
+print(want_tostudy ,type(want_tostudy))
+xyz = None
+print(xyz , type(xyz))
+a = [5,13,43,"python"]
+print(a ,type(a))
+a = {5,13,43,"python"}
+print(a ,type(a))
+a = (5,13,43,"python")
+print(a ,type(a))
+car_info = {
+    "brand" : "mercedes",
+    "colour" : "black",
+    "year" : 1994 
+}
+print(car_info)
