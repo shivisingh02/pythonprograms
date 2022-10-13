6f5e6de5967f770715390d28bc591d19f384d840
shivisingh02 <111993202+shivisingh02@users.noreply.github.com>
2022-10-13 20:08:43 +0530

    data mining

--- c2.py
+++ c2.py
@@ -11,3 +11,4 @@ cat1 = cat('soni' , 2 , 'persian')
 cat2 = cat('mia' , 2 , 'siamese')
 
 print(cat1.name , cat1.age ,cat1.breed)
+print(cat2.name , cat2.age , cat2.breed)
---
+++ data_mining/latest_news_bot.py
@@ -0,0 +1,54 @@
+import requests 
+from bs4 import BeautifulSoup
+import os
+import pandas as pd 
+import datetime
+
+def get_soup(url):
+    r = requests.get(url)
+    if r.status_code == 200:
+        return BeautifulSoup(r.text)
+    return None
+
+def get_news(item):
+    data = {}
+    heading = item.find('h2' , class_ = 'newsHong')
+    source = item.find('span' , class_ = 'posted-by')
+    content = item.find('p' , class_ = 'newsCont')
+    if heading is not None:
+        data['heading'] = heading.text.strip()
+    if source is not None:
+        data['source'] = source.text.strip()
+    if content is not None:
+        data['conten'] = content.text.strip()
+    return data
+
+def get_news_list(soup):
+    headlines = []
+    news  = soup.find('div' , class_ = 'lisingNews')
+    for item in news.find_all('div' , class_ = 'news_Itm'):
+        headlines.append(get_news(item))
+    return headlines
+
+def main(filename = 'latest_news.csv'):
+    pos = 1 
+    base_url = "https://www.ndtv.com/latest"
+    all_headlines = []
+    while True:
+        url = f'{base_url}/page-{pos}'
+        print(' ' , url)
+        soup = get_soup(url)
+        if soup is None:
+            break
+        else:
+            headlines = get_news_list(soup)
+            if headlines:
+                all_headlines.extend(headlines)
+                pos += 1 
+    time = datetime.datetime.now().strftime("%Y-%m_%d_%H")
+    filename = f'{time}_{filename}'
+    pd.DataFrame(all_headlines).to_csv(filename, index = False)
+
+if __name__ == '__main__':
+    main()
+
